706b83dfa61f2a8c4347f0a0867fd231c607a0aa
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-01-29 15:23:22 +0530

    Upload dotfiles

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -23,5 +23,9 @@ vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
 vim.keymap.set({ "n", "v" }, "j", "gj", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "k", "gk", { noremap = true, silent = true })
 
+-- Change buffer
+vim.keymap.set("n", "[b", ":bprevious<CR>", { noremap = true, silent = true }) -- Go to previous buffer
+vim.keymap.set("n", "]b", ":bnext<CR>", { noremap = true, silent = true })     -- Go to next buffer
+
 -- Plugin
 vim.keymap.set({ "n", "v" }, "<leader>/", ":CommentToggle<CR>")
--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,11 +1,19 @@
-
-return {
+return { {
+  "hrsh7th/cmp-nvim-lsp"
+},
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     config = function()
       local cmp = require("cmp")
-
+      require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup({
         sources = {
           { name = "nvim_lsp" },
