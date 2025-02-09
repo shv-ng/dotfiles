606c3b3a689082e80e23de0793b2055c4bcfc7e5
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-02-09 23:50:55 +0530

    Modified push.sh

--- push.sh
+++ push.sh
@@ -1,19 +1,26 @@
+#!/bin/bash
 
-# Copy from ~/.config to ~/dotfiles
+# Get list of changed files
+git status --porcelain | while read -r status file; do
+    # Extract file status
+    change_type=$(echo "$status" | awk '{print $1}')
+    
+    # Generate commit message based on status
+    case "$change_type" in
+        M) commit_msg="Modified $file" ;;
+        A) commit_msg="Added $file" ;;
+        D) commit_msg="Deleted $file" ;;
+        "??") commit_msg="Untracked $file" ;; # Optional, can ignore if not needed
+        *) commit_msg="Updated $file" ;;
+    esac
 
-# Remove existing dotfiles
-rm -rf .config
-rm -rf .mozilla
+    # Add, commit, and push
+    git add "$file"
+    git commit -m "$commit_msg"
+done
 
-# Python script to copy from .config to dotfiles/config
-python3 copy_to_dotfiles.py
+# Push all committed changes
+git push origin "$(git branch --show-current)"
 
-
-# Remove pycache
-rm -rf __pycache__/
-
-# Push to github
-git add .
-git commit -m "$1"
-git push
+echo "All changes committed and pushed successfully!"
 
