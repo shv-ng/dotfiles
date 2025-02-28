3bae8039f1ea671e1a6755124b8c5321b09ecfa0
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-02-28 15:10:45 +0530

    Modified .config/zsh/functions/fuzzy_cd.sh

--- .config/zsh/functions/fuzzy_cd.sh
+++ .config/zsh/functions/fuzzy_cd.sh
@@ -1,10 +1,10 @@
+
 fcda(){
-    cd "$(find -type d | fzf)"
+   cd "$(fd --full-path -H --type d | fzf)"
 }
 
 fcd() {
     cd "$(fd --type d \
-        --exclude .git \
         --exclude target \
         --exclude build \
         --exclude node_modules \
