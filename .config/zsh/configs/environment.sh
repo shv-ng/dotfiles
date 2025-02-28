6254fe7874418979b31b36ba2bef79e4b29fe5e5
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-02-28 19:00:54 +0530

    Modified .config/zsh/configs/environment.sh

--- .config/zsh/configs/environment.sh
+++ .config/zsh/configs/environment.sh
@@ -2,6 +2,7 @@
 export ZSH="$HOME/.oh-my-zsh"
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 export TMUX_CONF="$HOME/.config/tmux/tmux.conf"
-export PATH="$HOME/.local/share/nvim/mason/bin:$PATH"
+export MY_BIN="$HOME/bin"
+export PATH="$HOME/.local/share/nvim/mason/bin:$PATH:$MY_BIN"
 
 source "$HOME/.local/bin/env"
