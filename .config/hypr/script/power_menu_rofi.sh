bb17abb6eed21b88f951260fdf06ca972b11d737
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-03-05 17:11:44 +0530

    Modified .config/hypr/script/power_menu_rofi.sh

--- .config/hypr/script/power_menu_rofi.sh
+++ .config/hypr/script/power_menu_rofi.sh
@@ -10,18 +10,13 @@ selection=$(echo -e "$options" | rofi -dmenu -p "Select an action")
 # Execute commands based on the selected option
 case "$selection" in
     "Power off")
-        dunstify "Shutting down..."   # Notify before shutdown
         systemctl poweroff            # Shutdown command
         ;;
     "Restart")
-        dunstify "Restarting..."      # Notify before restart
         systemctl reboot              # Restart command
         ;;
     "Suspend")
-        dunstify "Suspending..."      # Notify before suspend
         systemctl suspend             # Suspend command
         ;;
     *)
-        dunstify "Invalid selection or cancelled."  # Notify on invalid/cancelled selection
-        ;;
 esac
