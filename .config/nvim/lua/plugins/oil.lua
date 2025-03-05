032c610e1bfe7aef6144afaa042df2bb91c0fd9b
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-03-05 17:11:44 +0530

    Modified .config/nvim/lua/plugins/oil.lua

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,43 @@ return {
 
   config = function()
     local oil = require("oil")
-    oil.setup()
-    vim.keymap.set("n", "-", oil.toggle_float, {})
+    local fzf = require("fzf-lua")
+    oil.setup({
+      keymaps = {
+        ["<C-v>"] = { "actions.preview", opts = { split = "botright" } },
+        ["<C-g>"] = function()
+          fzf.fzf_exec("fd --type d --hidden --follow --exclude .git", {
+            prompt = "Find Directory: ",
+            actions = {
+              ["default"] = function(selected)
+                oil.open(selected[1])
+              end
+            }
+          })
+        end,
+        ["<C-p>"] = function()
+          fzf.fzf_exec("fd --type f --hidden --follow --exclude .git", {
+            prompt = "Find File: ",
+            actions = {
+              ["default"] = function(selected)
+                oil.open(selected[1])
+              end
+            }
+          })
+        end,
+        ["<leader>ff"] = function()
+          fzf.files({ cwd = oil.get_current_dir() })
+        end,
+      },
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          local m = name:match('^..$')
+          return m ~= nil
+        end
+      },
+    })
+    vim.keymap.set("n", "-", ":Oil<CR>", {})
+    vim.cmd("Oil")
   end,
 }
