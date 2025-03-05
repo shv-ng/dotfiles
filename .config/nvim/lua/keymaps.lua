10bc387fc1a1429ef64a8928069e3efc8e58e56c
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-03-06 01:48:05 +0530

    Modified .config/nvim/lua/keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,56 +1,55 @@
--- space bar leader key
-vim.g.mapleader = " "
+-- Set indentation and tab behavior
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.tabstop = 2      -- Tab width of 2 spaces
+vim.opt.softtabstop = 2  -- Backspace/delete treats tab as 2 spaces
+vim.opt.shiftwidth = 2   -- Indentation width of 2 spaces
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+-- General settings
+vim.opt.swapfile = false -- Disable swap files
+vim.opt.scrolloff = 8    -- Keep 8 lines visible when scrolling
+vim.g.mapleader = " "    -- Set space as the leader key
 
+-- Window navigation shortcuts
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
-vim.opt.swapfile = false
+-- Copy/paste using system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- Copy
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]]) -- Paste
 
--- Navigate vim panes better
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
-
--- Copy/paste to/from clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
-
--- Movement in wrapped line
+-- Smooth movement in wrapped lines
 vim.keymap.set({ "n", "v" }, "j", "gj", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "k", "gk", { noremap = true, silent = true })
 
--- Change buffer
-vim.keymap.set("n", "<leader>[", ":bprevious<CR>", { noremap = true, silent = true }) -- Go to previous buffer
-vim.keymap.set("n", "<leader>]", ":bnext<CR>", { noremap = true, silent = true })     -- Go to next buffer
+-- Buffer navigation
+vim.keymap.set("n", "<leader>[", ":bprevious<CR>", { silent = true }) -- Previous buffer
+vim.keymap.set("n", "<leader>]", ":bnext<CR>", { silent = true })     -- Next buffer
 
--- Tab
-vim.keymap.set("n", "<leader>tc", ":tabnew<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { noremap = true, silent = true })
+-- Tab management
+vim.keymap.set("n", "<leader>tc", ":tabnew<CR>", { silent = true })      -- New tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", { silent = true })    -- Close tab
+vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { silent = true }) -- Prev tab
+vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { silent = true })     -- Next tab
 
+-- FzfLua shortcuts
+vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { silent = true })                -- Search files
+vim.keymap.set("n", "<leader><leader>", ":FzfLua live_grep<CR>", { silent = true }) -- Live grep
+vim.keymap.set("n", "<leader>fg", ":FzfLua buffers<CR>", { silent = true })         -- Search buffers
+vim.keymap.set("n", "<leader>fl", ":FzfLua<CR>", { silent = true })                 -- Open FzfLua
 
-
-
--- Fzf Lua
-vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { silent = true })
-vim.keymap.set("n", "<leader><leader>", ":FzfLua live_grep<CR>", { silent = true })
-vim.keymap.set("n", "<leader>fg", ":FzfLua buffers<CR>", { silent = true })
-vim.keymap.set("n", "<leader>fl", ":FzfLua<CR>", { silent = true })
-
+-- Move selected lines up/down in visual mode
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- Keep cursor centered when scrolling/searching
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- Make current file executable
 vim.keymap.set("n", "<leader>x", function()
   vim.cmd("!chmod +x " .. vim.fn.expand("%"))
 end)
-
-vim.keymap.set("x", "j", "jzz")
-vim.keymap.set("x", "k", "kzz")
