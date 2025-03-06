713ee69638fc0f91597154505e205985fd6ce7d4
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-03-07 02:46:18 +0530

    Modified .config/nvim/lua/plugins/completions.lua

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,28 +1,46 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets',
-    { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-  },
-
-  version = '*',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = {
-      preset = 'default',
-      ['<C-y>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-space>'] = { 'select_and_accept' },
-    },
-
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
-    },
-    snippets = { preset = 'luasnip' },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
     },
   },
-  opts_extend = { "sources.default" }
-}
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets',
+      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+    },
+
+    version = '*',
+
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<C-y>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-space>'] = { 'select_and_accept' },
+      },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+      snippets = { preset = 'luasnip' },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', "lazydev" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
+    },
+    opts_extend = { "sources.default" }
+  }, }
