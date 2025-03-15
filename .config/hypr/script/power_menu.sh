84aba5a48cdda09d332f25d6c65268318eec1052
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-03-15 14:36:13 +0530

    Modified: .config/hypr/script/power_menu.sh

--- .config/hypr/script/power_menu.sh
+++ .config/hypr/script/power_menu.sh
@@ -2,7 +2,6 @@
 
 options="Power off\nRestart\nSuspend\nEnable autologin\nDisable autologin"
 
-
 AUTO_LOGIN_DIR="/etc/systemd/system/getty@tty1.service.d"
 AUTO_LOGIN_FILE="$AUTO_LOGIN_DIR/autologin.conf"
 
@@ -18,14 +17,13 @@ selection=$(echo -e $options | fzf --prompt="$AUTO_LOGIN_MESSAGE Select an actio
 
 case "$selection" in
     "Power off")
-        systemctl   poweroff         # Shutdown command
+        systemctl poweroff
         ;;
     "Restart")
-        systemctl reboot              # Restart command
+        systemctl reboot
         ;;
-
     "Suspend")
-        systemctl suspend             # Suspend command
+        systemctl suspend
         ;;
     "Enable autologin")
         if [[ ! -f $AUTO_LOGIN_FILE ]]; then
@@ -33,8 +31,7 @@ case "$selection" in
             exit 0
         fi
 
-
-    COUNT_HASH=$(cat $AUTO_LOGIN_FILE | rg -c "#")
+        COUNT_HASH=$(cat $AUTO_LOGIN_FILE | rg -c "#")
         if [[ $COUNT_HASH -gt 0 ]]; then
             sudo sed -i "s/#//g" $AUTO_LOGIN_FILE
             sudo systemctl daemon-reload
@@ -43,16 +40,13 @@ case "$selection" in
             notify-send "Autologin already enabled"
 
         fi
-
-
         ;;
     "Disable autologin") if [[ ! -f $AUTO_LOGIN_FILE ]]; then
             notify-send "No autologin file found, create it first" -u critical
             exit 0
         fi
 
-
-    COUNT_HASH=$(cat $AUTO_LOGIN_FILE | rg -c "#")
+        COUNT_HASH=$(cat $AUTO_LOGIN_FILE | rg -c "#")
         if [[ $COUNT_HASH -eq 0 ]]; then
             sudo sed -i "s/ExecStart/#ExecStart/g" $AUTO_LOGIN_FILE
             sudo systemctl daemon-reload
@@ -60,7 +54,6 @@ case "$selection" in
         else
             notify-send "Autologin already disabled"
         fi
-
         ;;
     *)
 esac
