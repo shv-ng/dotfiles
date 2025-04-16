050ea452e1bf324aad17c3e5a983b4156d907107
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-04-16 15:30:12 +0530

    Modified: .config/htop/htoprc .config/hypr/conf/binds.conf files_to_push.txt; Deleted: .mozilla/firefox/kfxpgy35.default-release-1733027217126/chrome/userChrome.css; Untracked: .config/hypr/script/open_browser.sh .mozilla/firefox/8461zk2u.default-release-1744796796185/

---
+++ .config/hypr/script/open_browser.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+website=$(echo -e 'Whatsapp\nYouTube\nLinkedIn\nChatGPT\nGitHub\nYoutube Music\nGMail\nGoogle Docs\nGoogle Sheets' | fzf --layout reverse --border ) 
+
+case "$website" in
+  "Whatsapp") url="https://web.whatsapp.com" ;;
+  "YouTube") url="https://youtube.com" ;;
+  "LinkedIn") url="https://linkedin.com" ;;
+  "ChatGPT") url="https://chatgpt.com" ;;
+  "GitHub") url="https://github.com/ShivangSrivastava" ;;
+  "Youtube Music") url="https://music.youtube.com" ;;
+  "GMail") url="https://mail.google.com" ;;
+  "Google Docs") url="https://docs.google.com/document/u/0/" ;;
+  "Google Sheets") url="https://docs.google.com/spreadsheets/u/0/" ;;
+  *) 
+    url=""
+        ;;
+esac
+
+hyprctl dispatch workspace 2
+if pgrep firefox > /dev/null; then
+  xdg-open "$url"
+else
+  if [ -z $url ]; then 
+    nohup firefox >/dev/null 2>&1 &
+  else
+    nohup firefox "$url" >/dev/null 2>&1 &
+  fi
+  sleep 3
+fi
