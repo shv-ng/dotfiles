d0af2287633635bdeaea56f30dc2e8489c881af0
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-04-16 15:36:05 +0530

    Modified: .config/hypr/script/open_browser.sh

--- .config/hypr/script/open_browser.sh
+++ .config/hypr/script/open_browser.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-website=$(echo -e 'Whatsapp\nYouTube\nLinkedIn\nChatGPT\nGitHub\nYoutube Music\nGMail\nGoogle Docs\nGoogle Sheets' | fzf --layout reverse --border ) 
+current_ws=$(hyprctl activeworkspace -j | jq -r '.id')
+
+if [ "$current_ws" -eq 2 ]; then
+    if pgrep firefox > /dev/null; then
+        firefox --new-window
+    else
+        nohup firefox >/dev/null 2>&1 &
+    fi
+    sleep 0.5
+    exit 0
+fi
+
+website=$(echo -e 'Whatsapp\nYouTube\nLinkedIn\nChatGPT\nGitHub\nYoutube Music\nGMail\nGoogle Docs\nGoogle Sheets' | fzf --layout reverse --border )
 
 case "$website" in
-  "Whatsapp") url="https://web.whatsapp.com" ;;
-  "YouTube") url="https://youtube.com" ;;
-  "LinkedIn") url="https://linkedin.com" ;;
-  "ChatGPT") url="https://chatgpt.com" ;;
-  "GitHub") url="https://github.com/ShivangSrivastava" ;;
-  "Youtube Music") url="https://music.youtube.com" ;;
-  "GMail") url="https://mail.google.com" ;;
-  "Google Docs") url="https://docs.google.com/document/u/0/" ;;
-  "Google Sheets") url="https://docs.google.com/spreadsheets/u/0/" ;;
-  *) 
-    url=""
-        ;;
+    "Whatsapp") url="https://web.whatsapp.com" ;;
+    "YouTube") url="https://youtube.com" ;;
+    "LinkedIn") url="https://linkedin.com" ;;
+    "ChatGPT") url="https://chatgpt.com" ;;
+    "GitHub") url="https://github.com/ShivangSrivastava" ;;
+    "Youtube Music") url="https://music.youtube.com" ;;
+    "GMail") url="https://mail.google.com" ;;
+    "Google Docs") url="https://docs.google.com/document/u/0/" ;;
+    "Google Sheets") url="https://docs.google.com/spreadsheets/u/0/" ;;
+    *) url="" ;;
 esac
 
 hyprctl dispatch workspace 2
 if pgrep firefox > /dev/null; then
-  xdg-open "$url"
+    xdg-open "$url"
 else
-  if [ -z $url ]; then 
-    nohup firefox >/dev/null 2>&1 &
-  else
-    nohup firefox "$url" >/dev/null 2>&1 &
-  fi
-  sleep 3
+    if [ -z $url ]; then
+        nohup firefox >/dev/null 2>&1 &
+    else
+        nohup firefox "$url" >/dev/null 2>&1 &
+    fi
+    sleep 0.5
 fi
