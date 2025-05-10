b0e3f57a6ecae5c1060e90298037589b6d7a0276
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-10 08:27:34 +0530

    Modified: .config/zsh/functions/tom.sh .mozilla/firefox/vpa7a4p0.dev-edition-default-1/chrome/userChrome.css

--- .config/zsh/functions/tom.sh
+++ .config/zsh/functions/tom.sh
@@ -2,9 +2,13 @@
 tom(){
     if [[ -z "$(rg tomlscript pyproject.toml )" ]]; then
         echo -e "\n[tool.tomlscript]"\
+            "\n# runserver"\
             "\ndev = \"uv run manage.py runserver\""\
+            "\n# manage.py"\
             "\nmanage = \"uv run manage.py\""\
+            "\n# makemigrations and migrate"\
             "\nmigrate = \"uv run manage.py makemigrations && uv run manage.py migrate\""\
+            "\n# startapp"\
             "\nstartapp = \"uv run manage.py startapp\"">> pyproject.toml
     fi
     uvx tomlscript "$@"
