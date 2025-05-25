5430e233dfa0d73979293fc7c23ec01a3951052f
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-25 22:28:23 +0530

    refactor: commented neocodeium

--- .config/nvim/lua/plugins/ai-completion.lua
+++ .config/nvim/lua/plugins/ai-completion.lua
@@ -1,9 +1,9 @@
 return {
-  "monkoose/neocodeium",
-  event = "VeryLazy",
-  config = function()
-    local neocodeium = require("neocodeium")
-    neocodeium.setup()
-    vim.keymap.set("i", "<A-f>", neocodeium.accept)
-  end,
+  -- "monkoose/neocodeium",
+  -- event = "VeryLazy",
+  -- config = function()
+  --   local neocodeium = require("neocodeium")
+  --   neocodeium.setup()
+  --   vim.keymap.set("i", "<A-f>", neocodeium.accept)
+  -- end,
 }
