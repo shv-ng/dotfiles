ddd14dabe9c586e6ca0cf18bf153ffa89e5c04cc
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-25 22:30:55 +0530

    feat: Added go path

--- .config/zsh/configs/environment.sh
+++ .config/zsh/configs/environment.sh
@@ -1,5 +1,6 @@
 # Set environment variables
 export ZSH="$HOME/.oh-my-zsh"
+export GOPATH="$HOME/go"
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 export TMUX_CONF="$HOME/.config/tmux/tmux.conf"
 export PERSONAL="$HOME/personal"
@@ -7,6 +8,7 @@ export PATH="$HOME/.local/share/nvim/mason/bin:$PATH:$MY_BIN"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH=$PATH:/usr/local/go/bin
 export PGDATA=~/pgdata
+export PATH=$PATH:$GOPATH/bin
 # source "$HOME/.local/bin/env"
 
 alias firefox="firefox-developer-edition"
