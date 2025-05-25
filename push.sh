ae59231f80ab3eca5eb91e2270125588f6134b54
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-25 22:32:49 +0530

    feat: use lazygit for better comments

--- push.sh
+++ push.sh
@@ -11,39 +11,5 @@ python3 make_copy.py
 # Remove pycache
 rm -rf __pycache__/
 
-# Initialize arrays for each type of change
-modified=()
-added=()
-deleted=()
-untracked=()
-
-# Get the list of changed files
-while read -r status file; do
-    change_type=$(echo "$status" | awk '{print $1}')
-
-    case "$change_type" in
-        M) modified+=("$file") ;;
-        A) added+=("$file") ;;
-        D) deleted+=("$file") ;;
-        "??") untracked+=("$file") ;;
-    esac
-
-    # Add file to staging
-    git add "$file"
-done < <(git status --porcelain)
-
-# Construct commit message
-commit_msg=""
-
-[[ ${#added[@]} -gt 0 ]] && commit_msg+="Added: ${added[*]}; "
-[[ ${#modified[@]} -gt 0 ]] && commit_msg+="Modified: ${modified[*]}; "
-[[ ${#deleted[@]} -gt 0 ]] && commit_msg+="Deleted: ${deleted[*]}; "
-[[ ${#untracked[@]} -gt 0 ]] && commit_msg+="Untracked: ${untracked[*]}; "
-
-# Remove trailing semicolon and space
-commit_msg=${commit_msg%"; "}
-
-# Commit and push if there are changes
-git commit -m "$commit_msg"
-git push origin "$(git branch --show-current)"
-
+# Let me do remaining with lazygit
+lazygit
